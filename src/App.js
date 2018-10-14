4459483b56ab3bb38c94991510005470acaeb6f1
Markus Klinga <klinga.markus@gmail.com>
2018-10-14 20:22:59 +0200

    Add some styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 import PhoneListContainer from "./components/PhoneListContainer";
+
+const AppContainer = styled.div`
+  max-width: 1000px;
+  margin: auto;
+  background: white;
+  height: 100vh;
+  min-height: 100vh;
+`;
+
+const Header = styled.header`
+  padding: 10px 50px;
+`;
+
+const Title = styled.h1`
+  color: #333333;
+  border-bottom: 5px solid #1fb9cf;
+  text-align: right;
+`;
+
+const MainContainer = styled.main`
+  padding: 10px 50px;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Phone viewer</h1>
-        </header>
-        <main>
+      <AppContainer>
+        <Header>
+          <Title>Phone viewer</Title>
+        </Header>
+        <MainContainer>
           <PhoneListContainer phones={this.props.phones} />
-        </main>
-      </div>
+        </MainContainer>
+      </AppContainer>
     );
   }
 }
