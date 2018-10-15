376b878fd75a9762914aacd8fe2142fe47ef2370
Markus Klinga <klinga.markus@gmail.com>
2018-10-15 20:32:30 +0200

    Remove unnecessary prop

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default class App extends Component {
           <Title>Phone viewer</Title>
         </Header>
         <MainContainer>
-          <PhoneListContainer phones={this.props.phones} />
+          <PhoneListContainer />
         </MainContainer>
       </AppContainer>
     );
